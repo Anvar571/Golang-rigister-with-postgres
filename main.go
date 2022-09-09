7c63550ba625055c686e03879aee3737acfff0db
Anvar571 <abdurashidovanvar52@gmail.com>
2022-09-09 15:23:11 +0500

    working...

--- main.go
+++ main.go
@@ -15,6 +15,30 @@ const (
 	password = "1"
 )
 
+type EmployeS interface {
+	Connect()
+	Rigister()
+	Login()
+	
+}
+
+type Rigister struct {
+	f_name string
+	lastname string
+	parol int
+	age int
+	job string
+}
+
+func (r *Rigister) rigister(name, lastname, job string, parol, age int) string {
+	query := `INSERT INTO rigister(f_name, lastname, parol, age, job)VALUES
+	('anvar', 'abdurashidov', '123', '21', 'developer')`
+	_, e := db.Exec(query)
+	if e != nil {
+		panic(e)
+	}
+}
+
 func main() {
 	dbSql := fmt.Sprintf("host=%s port=%d user=%s password=%s database=%s", host, port, user, password, database)
 	db, err := sql.Open("postgres", dbSql)
@@ -27,5 +51,10 @@ func main() {
 		log.Fatal(error)
 	}
 
-	fmt.Println("connection success")
+	query := `INSERT INTO rigister(f_name, lastname, parol, age, job)VALUES
+	('anvar', 'abdurashidov', '123', '21', 'developer')`
+	_, e := db.Exec(query)
+	if e != nil {
+		panic(e)
+	}
 }

bcbea63c57fd29f21a855bdacbbe3e49d60d934d
Anvar571 <abdurashidovanvar52@gmail.com>
2022-09-10 22:26:34 +0500

    reading database fineshed

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	_ "github.com/lib/pq"
 	"log"
 	"strings"
-	_ "github.com/lib/pq"
 )
 
 const (
@@ -42,16 +42,14 @@ type Rigister struct {
 func (r *Rigister) rigister(f_name, lastname, job string, parol, age int) {
 	data := strings.Split((fmt.Sprintf("%s %s %s %d %d", f_name, lastname, job, parol, age)), " ")
 	query := `INSERT INTO rigister(f_name, lastname, job, parol, age)VALUES($1, $2, $3, $4, $5)`
+	fmt.Println(data)
 	_, e := db.Exec(query, data[0], data[1], data[2], data[3], data[4])
 	if e != nil {
 		panic(e)
 	}
 }
 
-func main() {
-	defer db.Close()
-	Connect()
-	var user = Rigister{}
+func (r *Rigister) show() {
 	var (
 		f_name string
 		lastname string
@@ -59,19 +57,58 @@ func main() {
 		parol int
 		age int
 	)
-
-	fmt.Println("Enter first name")
-	fmt.Scan(&f_name)
-	fmt.Println("Enter last name")
-	fmt.Scan(&lastname)
-	fmt.Println("Enter job name")
-	fmt.Scan(&job)
-	fmt.Println("Enter parol name")
-	fmt.Scan(&parol)
-	fmt.Println("Enter age name")
-	fmt.Scan(&age)
+	readQuereData := `SELECT f_name, lastname, job, parol, age from rigister`
+	row, err := db.Query(readQuereData)
+	if err != nil {
+		panic(err)
+	}
+	defer row.Close()
+	for row.Next() {
+		err = row.Scan(&f_name, &lastname, &job, &parol, &age)
+		switch err {
+		case sql.ErrNoRows:
+			fmt.Println("no row ")
+		case nil:
+			fmt.Println(f_name, lastname, job, parol, age)
+		default:
+			fmt.Println("xatolik yuz berdi")
+		}
+	}
 	
-	user.rigister(f_name, lastname, job, parol, age)
 }
 
-// f_name, lastname, job string, parol, age int
+func main() {
+	defer db.Close()
+	Connect()
+	var user = Rigister{}
+	for {
+		fmt.Println("[1]- Register\n[2]- Show data")
+		var son int
+		fmt.Scan(&son)
+		switch son {
+		case 1:
+			var (
+				f_name   string
+				lastname string
+				job      string
+				parol    int
+				age      int
+			)
+	
+			fmt.Println("Enter first name")
+			fmt.Scan(&f_name)
+			fmt.Println("Enter last name")
+			fmt.Scanf("%s", &lastname)
+			fmt.Printf("Enter job name")
+			fmt.Scanf("%s", &job)
+			fmt.Println("Enter parol name")
+			fmt.Scanln(&parol)
+			fmt.Println("Enter age name")
+			fmt.Scanln(&age)
+	
+			user.rigister(f_name, lastname, job, parol, age)
+		case 2:
+			user.show()
+		}
+	}
+}
